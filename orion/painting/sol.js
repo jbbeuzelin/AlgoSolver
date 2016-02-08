8a1038837317c10df53f3f5b19734eaed16a4852
JeromeCh06 <jerome.chaaban@gmail.com>
2016-02-08 01:38:55 +0100

    Painting algo v2

--- orion/painting/sol.js
+++ orion/painting/sol.js
@@ -26,35 +26,30 @@ var test = (function (_super) {
                     return 1;
                 else if (element == ".")
                     return 0;
+                //Return -1 pour les caractères de fin de ligne qui seront du coup exclus grâce au _.filter
                 return -1;
             }), function (element) {
                 return element != -1;
             });
             this.grid.push(ligne);
         }
+        //Il faut déjà écrire en entête du fichier le nombre d'opérations, qu'on ne connait pas avant d'avoir résolu !
         this.toWrite.push("0");
+        /*
+        * Premier algo ou deuxième algo
+        */
         //this.jeRemplisCaseParCaseLes1();
-        this.jeMetsDesCarresSansFaireDErreurs();
+        //this.jeMetsDesCarresSansFaireDErreurs();
+        this.jeMetsDesCarresEtDesLignesEnPermettantDesErreurs();
+        this.nettoyerGrille();
+        console.log("Nb opérations", this.toWrite.length - 1);
+        //A présent on connait le nombre d'opérations
         this.toWrite[0] = "" + (this.toWrite.length - 1);
+        //On n'a plus qu'à écrire dans le fichier
         _.forEach(this.toWrite, function (element) {
             _this.writer.writeToBuffer(element);
         });
         var _a;
-        /*let length = _.parseInt(this.reader.nextLine());
-        let numbers = [];
-        numbers = _.map(this.reader.nextLine().split(' '), _.parseInt);
-        let orderedNumbers = _.sortBy(numbers);
-        let swapsNumber = 0;
-        
-        _.forEach(orderedNumbers, n => {
-            let numberIndex = _.indexOf(numbers, n);
-            swapsNumber += Math.min(numberIndex, length - 1 - numberIndex);
-            
-            numbers.splice(numberIndex, 1);
-            length--;
-        });
-                  
-        this.writer.writeToBuffer(`Case #${testNumber}: ${swapsNumber}`);*/
     };
     test.prototype.jeRemplisCaseParCaseLes1 = function () {
         for (var i = 0; i < this.R; i++) {
@@ -79,16 +74,20 @@ var test = (function (_super) {
                             for (var jj = -k; jj <= k; jj++) {
                                 var iii = i + ii;
                                 var jjj = j + jj;
+                                //On s'autorise à repasser sur une case déjà colorée avec le %10. Mais on part toujours d'une case qui n'a jamais été touché grâce au if this.grid[i][j]==1.
                                 if (iii < 0 || jjj < 0 || iii >= this.R || jjj >= this.C || this.grid[iii][jjj] % 10 != 1) {
                                     ok = false;
                                     break checkCaree;
                                 }
                             }
                         }
+                        //Si non ok, cela signifie que le carré n'est pas colorer.
                         if (ok) {
+                            //Si les carrés sont de taille intéressante, alors on affiche quel carré on colore. Sinon on s'en branle, il y en a des milliers
                             if (k >= 7) {
                                 console.log("On écrit", "PAINT_SQUARE " + i + " " + j + " " + k);
                             }
+                            //On colore les cases à colorer
                             for (var ii = -k; ii <= k; ii++) {
                                 for (var jj = -k; jj <= k; jj++) {
                                     var iii = i + ii;
@@ -96,13 +95,161 @@ var test = (function (_super) {
                                     this.grid[iii][jjj] += 10;
                                 }
                             }
+                            //On écrit la commande
                             this.toWrite.push("PAINT_SQUARE " + i + " " + j + " " + k);
                         }
                     }
                 }
             }
         }
-        console.log("Nb opérations", this.toWrite.length - 1);
+    };
+    test.prototype.jeMetsDesCarresEtDesLignesEnPermettantDesErreurs = function () {
+        var tailleMaximum = 300;
+        for (var k = tailleMaximum; k >= 0; k--) {
+            console.log("Analyse pour taille = ", k);
+            for (var i = 0; i < this.R; i++) {
+                for (var j = 0; j < this.C; j++) {
+                    if (k <= 15) {
+                        //Carré
+                        this.gererCarre(k, i, j);
+                    }
+                    //Ligne verticale
+                    this.gererLigne(k, 0, i, j);
+                    //Ligne horizontale
+                    this.gererLigne(0, k, i, j);
+                }
+            }
+        }
+    };
+    test.prototype.gererCarre = function (k, i, j) {
+        if (true || this.grid[i][j] == 1) {
+            var nb1 = 0;
+            var nb0 = 0;
+            var ok = true;
+            checkCaree: for (var ii = -k; ii <= k; ii++) {
+                for (var jj = -k; jj <= k; jj++) {
+                    var iii = i + ii;
+                    var jjj = j + jj;
+                    if (iii < 0 || jjj < 0 || iii >= this.R || jjj >= this.C) {
+                        ok = false;
+                        break checkCaree;
+                    }
+                    if (this.grid[iii][jjj] === 1) {
+                        nb1++;
+                    }
+                    if (this.grid[iii][jjj] === 0) {
+                        nb0++;
+                    }
+                }
+            }
+            //Si non ok, cela signifie que le carré n'est pas colorer.
+            if (ok && this.ouiOnColoreCarre(k, nb1, nb0)) {
+                //Si les carrés sont de taille intéressante, alors on affiche quel carré on colore. Sinon on s'en branle, il y en a des milliers
+                if (k >= 7) {
+                    console.log("On écrit", "PAINT_SQUARE " + i + " " + j + " " + k, nb1, nb0);
+                }
+                this.colorerCarre(k, i, j);
+                this.toWrite.push("PAINT_SQUARE " + i + " " + j + " " + k);
+            }
+        }
+    };
+    test.prototype.gererLigne = function (ki, kj, i, j) {
+        //Il est débile de faire commencer une ligne pile sur une case qui n'est pas à colorer !
+        if (this.grid[i][j] % 10 !== 1) {
+            return null;
+        }
+        var ok = true;
+        var nb1 = 0;
+        var nb0 = 0;
+        checkLigne: for (var ii = 0; ii <= ki; ii++) {
+            for (var jj = 0; jj <= kj; jj++) {
+                var iii = i + ii;
+                var jjj = j + jj;
+                if (iii < 0 || jjj < 0 || iii >= this.R || jjj >= this.C) {
+                    ok = false;
+                    break checkLigne;
+                }
+                if (this.grid[iii][jjj] === 1) {
+                    nb1++;
+                }
+                if (this.grid[iii][jjj] === 0) {
+                    nb0++;
+                }
+            }
+        }
+        var k = Math.max(ki, kj);
+        if (ok && this.ouiOnColoreLigne(k, nb1, nb0)) {
+            if (k >= 100) {
+                console.log("On écrit", "PAINT_LINE " + i + " " + j + " " + k, nb1, nb0);
+            }
+            this.colorerLigne(ki, kj, i, j);
+            this.toWrite.push("PAINT_LINE " + i + " " + j + " " + (i + ki) + " " + (j + kj));
+        }
+    };
+    test.prototype.ouiOnColoreCarre = function (k, nb1, nb0) {
+        //Si ça apporte quelque chose sans plus détruire l'existant, on le fait.
+        if (nb0 == 0 && nb1 > 0) {
+            return true;
+        }
+        //Si k=0, on colorie et point barre uniquement si c'est un 1 qu'il faut colorer.
+        if (k == 0) {
+            return nb1 == 1;
+        }
+        var premiersK = [1, 2, 3, 5];
+        //On a forcément au moins un 0. Donc une fausse note.
+        var nbCases = Math.pow(2 * k + 1, 2);
+        if (nb1 > nb0 && (nb1 > nbCases * 0.4) && ((k < premiersK.length && nb0 <= premiersK[k]) || (nb0 < (nbCases * 0.08)))) {
+            return true;
+        }
+        return false;
+    };
+    test.prototype.ouiOnColoreLigne = function (k, nb1, nb0) {
+        //Si ça apporte quelque chose sans plus détruire l'existant, on le fait.
+        if (nb0 == 0 && nb1 > 0) {
+            return true;
+        }
+        if (k == 0) {
+            return nb1 == 1;
+        }
+        //Si k = 0, c'est déjà traité, et on ne fait qqch que si la case est évidemment à colorer
+        //Si k = 1, alors il y a deux cases en question. Franchement, si 
+        //Si k = 2, on accepte qu'il y ait une case qui cloche. Mais bien sûr, il faut toujours 
+        var premiersK = [0, 0, 0, 1, 1];
+        //On a forcément au moins un 0. Donc une fausse note.
+        var nbCases = k + 1;
+        if (nb1 > nb0 && (nb1 > nbCases * 0.9) && ((k < premiersK.length && nb0 <= premiersK[k]) || (nb0 < (nbCases * 0.00)))) {
+            return true;
+        }
+        return false;
+    };
+    test.prototype.colorerCarre = function (k, i, j) {
+        for (var ii = -k; ii <= k; ii++) {
+            for (var jj = -k; jj <= k; jj++) {
+                var iii = i + ii;
+                var jjj = j + jj;
+                this.grid[iii][jjj] += 10;
+            }
+        }
+    };
+    //Si ki vaut quelque chose, alors on colore une ligne verticale. Sinon, c'est que l'on colore une ligne horizontale puisque le i est finalement constant.
+    test.prototype.colorerLigne = function (ki, kj, i, j) {
+        for (var ii = 0; ii <= ki; ii++) {
+            for (var jj = 0; jj <= kj; jj++) {
+                var iii = i + ii;
+                var jjj = j + jj;
+                this.grid[iii][jjj] += 10;
+            }
+        }
+    };
+    test.prototype.nettoyerGrille = function () {
+        for (var i = 0; i < this.R; i++) {
+            for (var j = 0; j < this.C; j++) {
+                //Il faut effacer car on a coloré alors qu'il aurait fallu laisser des 0.
+                if (this.grid[i][j] % 10 == 0 && this.grid[i][j] != 0) {
+                    this.toWrite.push("ERASE_CELL " + i + " " + j);
+                }
+            }
+        }
     };
     return test;
 })(BaseSolver_1.solver.BaseSolver);
--- orion/painting/sol.ts
+++ orion/painting/sol.ts
@@ -34,34 +34,35 @@ class test extends solver.BaseSolver {
                     return 1;
                 else if(element == ".")
                     return 0;
+                //Return -1 pour les caractères de fin de ligne qui seront du coup exclus grâce au _.filter
                 return -1
             }),element => {
                 return element != -1
             });
+            
             this.grid.push(ligne);
         }
+        
+        //Il faut déjà écrire en entête du fichier le nombre d'opérations, qu'on ne connait pas avant d'avoir résolu !
         this.toWrite.push("0");
+        
+        /*
+        * Premier algo ou deuxième algo
+        */
         //this.jeRemplisCaseParCaseLes1();
-        this.jeMetsDesCarresSansFaireDErreurs();
+        //this.jeMetsDesCarresSansFaireDErreurs();
+        this.jeMetsDesCarresEtDesLignesEnPermettantDesErreurs();
+        
+        this.nettoyerGrille();
+        console.log("Nb opérations",this.toWrite.length-1);
+        
+        //A présent on connait le nombre d'opérations
         this.toWrite[0] = ""+(this.toWrite.length-1);
+        
+        //On n'a plus qu'à écrire dans le fichier
         _.forEach(this.toWrite,element => {
             this.writer.writeToBuffer(element);
         });
-        /*let length = _.parseInt(this.reader.nextLine());
-        let numbers = [];
-        numbers = _.map(this.reader.nextLine().split(' '), _.parseInt);
-        let orderedNumbers = _.sortBy(numbers);
-        let swapsNumber = 0;
-        
-        _.forEach(orderedNumbers, n => {
-            let numberIndex = _.indexOf(numbers, n);
-            swapsNumber += Math.min(numberIndex, length - 1 - numberIndex);
-            
-            numbers.splice(numberIndex, 1);
-            length--;
-        });
-                  
-        this.writer.writeToBuffer(`Case #${testNumber}: ${swapsNumber}`);*/
     }
     
     jeRemplisCaseParCaseLes1(): void{
@@ -89,16 +90,22 @@ class test extends solver.BaseSolver {
                             for(let jj=-k; jj<=k; jj++){
                                 let iii = i+ii;
                                 let jjj = j+jj;
+                                
+                                //On s'autorise à repasser sur une case déjà colorée avec le %10. Mais on part toujours d'une case qui n'a jamais été touché grâce au if this.grid[i][j]==1.
                                 if(iii < 0 || jjj < 0 || iii >= this.R || jjj >= this.C || this.grid[iii][jjj] %10 != 1){
                                     ok = false;
                                     break checkCaree;
                                 }
                             }
                         }
+                        //Si non ok, cela signifie que le carré n'est pas colorer.
                         if(ok){
+                            //Si les carrés sont de taille intéressante, alors on affiche quel carré on colore. Sinon on s'en branle, il y en a des milliers
                             if(k>=7){
                                 console.log("On écrit","PAINT_SQUARE "+i+" "+j+" "+k);
                             }
+                            
+                            //On colore les cases à colorer
                             for(let ii=-k; ii<=k; ii++){
                                 for(let jj=-k; jj<=k; jj++){
                                     let iii = i+ii;
@@ -106,6 +113,8 @@ class test extends solver.BaseSolver {
                                     this.grid[iii][jjj] += 10;
                                 }
                             }
+                            
+                            //On écrit la commande
                             this.toWrite.push("PAINT_SQUARE "+i+" "+j+" "+k);
                         }
                         //Il peut y avoir un carré qui commence à partir d'ici
@@ -113,7 +122,165 @@ class test extends solver.BaseSolver {
                 }
             }
        }
-        console.log("Nb opérations",this.toWrite.length-1);
+    }
+    
+    jeMetsDesCarresEtDesLignesEnPermettantDesErreurs(): void{
+       let tailleMaximum = 300;
+       for(let k = tailleMaximum; k>=0; k--){
+           console.log("Analyse pour taille = ",k);
+           for(let i=0; i < this.R; i++){
+                for(let j=0; j < this.C; j++){
+                    if(k <= 15){
+                        //Carré
+                        this.gererCarre(k, i, j);
+                    }
+                    //Ligne verticale
+                    this.gererLigne(k, 0, i, j);
+                    //Ligne horizontale
+                    this.gererLigne(0, k, i, j);
+                }
+            }
+       }
+    }
+    
+    gererCarre(k, i, j): void{
+        if(true || this.grid[i][j] == 1){
+            let nb1 = 0;
+            let nb0 = 0;
+            let ok = true;
+            checkCaree:
+            for(let ii=-k; ii<=k; ii++){
+                for(let jj=-k; jj<=k; jj++){
+                    let iii = i+ii;
+                    let jjj = j+jj;
+                    
+                    if(iii < 0 || jjj < 0 || iii >= this.R || jjj >= this.C){
+                        ok = false;
+                        break checkCaree;
+                    }
+                    if(this.grid[iii][jjj] === 1){
+                        nb1++;
+                    }
+                    if(this.grid[iii][jjj] === 0){
+                        nb0++;
+                    }
+                }
+            }
+            //Si non ok, cela signifie que le carré n'est pas colorer.
+            if(ok && this.ouiOnColoreCarre(k,nb1,nb0)){
+                //Si les carrés sont de taille intéressante, alors on affiche quel carré on colore. Sinon on s'en branle, il y en a des milliers
+                if(k >= 7){
+                    console.log("On écrit","PAINT_SQUARE "+i+" "+j+" "+k,nb1,nb0);
+                }
+                this.colorerCarre(k,i,j);
+                this.toWrite.push("PAINT_SQUARE "+i+" "+j+" "+k);
+            }
+        }
+    }
+    
+    gererLigne(ki, kj, i, j): void{
+        //Il est débile de faire commencer une ligne pile sur une case qui n'est pas à colorer !
+        if(this.grid[i][j] % 10 !== 1){
+            return null;
+        }
+        let ok = true;
+        let nb1 = 0;
+        let nb0 = 0;
+        checkLigne:
+        for(let ii=0; ii <= ki; ii++){
+            for(let jj=0; jj <= kj; jj++){
+                let iii = i+ii;
+                let jjj = j+jj;
+                if(iii < 0 || jjj < 0 || iii >= this.R || jjj >= this.C){
+                    ok = false;
+                    break checkLigne;
+                }
+                if(this.grid[iii][jjj] === 1){
+                    nb1++;
+                }
+                if(this.grid[iii][jjj] === 0){
+                    nb0++;
+                }
+            }
+        }
+        let k = Math.max(ki,kj);
+        if(ok && this.ouiOnColoreLigne(k,nb1,nb0)){
+            if(k >= 100){
+                console.log("On écrit","PAINT_LINE "+i+" "+j+" "+k,nb1,nb0);
+            }
+            this.colorerLigne(ki,kj,i,j);
+            this.toWrite.push("PAINT_LINE "+i+" "+j+" "+(i+ki)+" "+(j+kj));
+        }
+    }
+    
+    ouiOnColoreCarre(k, nb1, nb0): boolean{
+        //Si ça apporte quelque chose sans plus détruire l'existant, on le fait.
+        if(nb0 == 0 && nb1 > 0){
+            return true;
+        }
+        //Si k=0, on colorie et point barre uniquement si c'est un 1 qu'il faut colorer.
+        if(k==0){
+            return nb1 == 1;
+        }
+        let premiersK = [1,2,3,5];
+        //On a forcément au moins un 0. Donc une fausse note.
+        let nbCases = Math.pow(2 * k +1,2);
+        if(nb1 > nb0 && (nb1 > nbCases * 0.4) && ((k < premiersK.length && nb0 <= premiersK[k]) || (nb0 < (nbCases * 0.08) ) )){
+            return true;
+        }
+        return false;
+    }
+    
+    ouiOnColoreLigne(k, nb1, nb0): boolean{
+        //Si ça apporte quelque chose sans plus détruire l'existant, on le fait.
+        if(nb0 == 0 && nb1 > 0){
+            return true;
+        }
+        if(k == 0){
+            return nb1 == 1;
+        }
+        //Si k = 0, c'est déjà traité, et on ne fait qqch que si la case est évidemment à colorer
+        //Si k = 1, alors il y a deux cases en question. Franchement, si 
+        //Si k = 2, on accepte qu'il y ait une case qui cloche. Mais bien sûr, il faut toujours 
+        let premiersK = [0,0,0,1,1];
+        //On a forcément au moins un 0. Donc une fausse note.
+        let nbCases = k+1;
+        if(nb1 > nb0 && (nb1 > nbCases * 0.9) && ((k < premiersK.length && nb0 <= premiersK[k]) || (nb0 < (nbCases * 0.00) ) )){
+            return true;
+        }
+        return false;
+    }
+    
+    colorerCarre(k, i, j): void{
+        for(let ii=-k; ii <= k; ii++){
+            for(let jj=-k; jj <= k; jj++){
+                let iii = i+ii;
+                let jjj = j+jj;
+                this.grid[iii][jjj] += 10;
+            }
+        }
+    }
+    
+    //Si ki vaut quelque chose, alors on colore une ligne verticale. Sinon, c'est que l'on colore une ligne horizontale puisque le i est finalement constant.
+    colorerLigne(ki, kj, i, j): void{
+        for(let ii=0; ii <= ki; ii++){
+            for(let jj=0; jj <= kj; jj++){
+                let iii = i+ii;
+                let jjj = j+jj;
+                this.grid[iii][jjj] += 10;
+            }
+        }
+    }
+    
+    nettoyerGrille(): void{
+        for(let i=0; i < this.R; i++){
+            for(let j=0; j < this.C; j++){
+                //Il faut effacer car on a coloré alors qu'il aurait fallu laisser des 0.
+                if(this.grid[i][j] % 10 == 0 && this.grid[i][j] != 0){
+                    this.toWrite.push("ERASE_CELL "+i+" "+j);
+                }
+            }
+        }
     }
 }
 
