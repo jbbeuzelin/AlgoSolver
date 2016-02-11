0c33c111bd08bcbd2f5111034ddaa00ef4263cb4
Riron <orioncharlier@gmail.com>
2016-02-11 20:09:36 +0100

    Revert "Correction"
    This reverts commit ad7514e8a81eae52939e58076c5bdb9eb6203460.

--- orion/hashcode2016round1/sol.js
+++ orion/hashcode2016round1/sol.js
@@ -38,58 +38,7 @@ var Hashcode2016round1 = (function (_super) {
         }
         var _a;
     };
-    // On passe le warehouse et les cliuents possibles + la charge du drone actulle
-    Hashcode2016round1.prototype.choosenCustomers = function (wareHouse, possibleCustomers, droneLoad) {
-        /*let sortedCustomers = _.sortBy(possibleCustomers, customer => {
-            return this.distance(this.positionsW[wareHouse], this.positionsC[customer]);
-        });*/
-        var _this = this;
-        if (droneLoad === void 0) { droneLoad = 0; }
-        var sortedCustomers = _.sortBy(possibleCustomers, function (customer) {
-            return _this.numberOfOrderedProductsByC[customer];
-        });
-        var choosenCustomers = [];
-        for (var i = 0; i < sortedCustomers.length; i++) {
-            var toAdd = 0;
-            var customerId = sortedCustomers[i];
-            for (var j = 0; j < this.numberOfOrderedProductsByC[customerId]; j++) {
-                var productId = this.orderedProductsByC[customerId][j];
-                toAdd += this.productsWeights[productId];
-                console.log('add', toAdd);
-            }
-            // Si c'est trop lourd on arrete tout de suite
-            // TODO essayer 2 / 3 cas de plus
-            if (droneLoad + toAdd > this.maximumLoad) {
-                break;
-            }
-            droneLoad += toAdd;
-            choosenCustomers.push(sortedCustomers[i]);
-        }
-        return choosenCustomers;
-    };
-    Hashcode2016round1.prototype.distance = function (pos0, pos1) {
-        return (pos0[0] - pos1[0]) * (pos0[0] - pos1[0]) + (pos0[1] - pos1[1]) * (pos0[1] - pos1[1]);
-    };
-    Hashcode2016round1.prototype.loadProductsInstructions = function (products, warehouse, drone) {
-        var _this = this;
-        var groups = _.groupBy(products);
-        _.forEach(groups, function (g) {
-            _this.writer.writeToBuffer(drone + " L " + warehouse + " " + g[0] + " " + g.length);
-        });
-    };
-    Hashcode2016round1.prototype.unloadProductsInstructions = function (products, warehouse, drone) {
-        var _this = this;
-        var groups = _.groupBy(products);
-        _.forEach(groups, function (g) {
-            _this.writer.writeToBuffer(drone + " U " + warehouse + " " + g[0] + " " + g.length);
-        });
-    };
-    Hashcode2016round1.prototype.deliverProduct = function (drone, customerId, product, numberOfProducts) {
-        this.writer.writeToBuffer(drone + " D " + customerId + " " + product + " " + numberOfProducts);
-    };
-    Hashcode2016round1.prototype.wait = function (drone, time) {
-        this.writer.writeToBuffer(drone + " W " + time);
-    };
     return Hashcode2016round1;
 })(BaseSolver_1.solver.BaseSolver);
 new Hashcode2016round1('practice');
+//# sourceMappingURL=sol.js.map
