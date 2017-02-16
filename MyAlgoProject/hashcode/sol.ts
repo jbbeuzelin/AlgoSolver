b359933ab9c0f9db67a83ddc74857da8252e5426
jbbeuzelin <jbbeuzelin.dev@gmail.com>
2017-02-16 19:39:39 +0100

    first hashcode commit

---
+++ MyAlgoProject/hashcode/sol.ts
@@ -0,0 +1,27 @@
+'use strict'
+
+import { parseInt as _parseInt, times } from 'lodash';
+
+import { BaseSolver } from '../../bin/core/BaseSolver';
+import { MathHelper } from '../../bin/core/Helper';
+
+class Hashcode extends BaseSolver {
+
+	constructor(fileName: string) {
+		super(fileName);
+
+		let testCases = _parseInt(this.reader.nextLine());
+
+		console.time('Time tacken by solver');
+		times(testCases, (i: number) => this.solveCase(i));
+		console.timeEnd('Time tacken by solver');
+
+		this.writer.writeFile();
+	}
+
+	solveCase(testCase: number): void {
+		this.writer.writeToBuffer(`Case #${testCase + 1}: ${testCase}`);
+	}
+}
+
+new Hashcode('./practice');
