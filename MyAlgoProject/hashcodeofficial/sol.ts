d5009d44c77a8bab3ac22de5ba04b562f632db4b
jbbeuzelin <jbbeuzelin.dev@gmail.com>
2017-02-23 18:41:24 +0100

    commit start orion

--- MyAlgoProject/hashcodeofficial/sol.ts
+++ MyAlgoProject/hashcodeofficial/sol.ts
@@ -10,7 +10,8 @@ class Hashcodeofficial extends BaseSolver {
 	constructor(fileName: string) {
 		super(fileName);
 
-		let testCases = _parseInt(this.reader.nextLine());
+		//let testCases = _parseInt(this.reader.nextLine());
+		let testCases = 1;
 
 		console.time('Time tacken by solver');
 		times(testCases, (i: number) => this.solveCase(i));
@@ -20,6 +21,8 @@ class Hashcodeofficial extends BaseSolver {
 	}
 
 	solveCase(testCase: number): void {
+		const [ A, B, C, D ] = this.reader.nextLine().split(' ').map(_parseInt);
+
 		this.writer.writeToBuffer(`Case #${testCase + 1}: ${testCase}`);
 	}
 }
